0f48fc541cbb13dd50c8ace680df613a2ee99162
haris.tiric <haris.tiric@weown.com>
2023-04-06 18:40:09 +0200

    generics

---
+++ c_sharp_in_depth/Lessons/Generics.cs
@@ -0,0 +1,19 @@
+﻿namespace c_sharp_in_depth.Lessons;
+
+public class Generics<T>
+{
+    public static List<T> GenerateNames()
+    {
+        var newList = new List<T>();
+        return newList;
+    }
+
+    public static void PrintNames(List<T> names) // List<string> names is parameter, when this function is called then names is called argument
+    {
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+    }
+    
+}
--- c_sharp_in_depth/Program.cs
+++ c_sharp_in_depth/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using c_sharp_in_depth.Lessons;
+
 Console.WriteLine("Hello, World!");
+
+var names = Generics<string>.GenerateNames();
+
+names.Add("Haris");
+names.Add("Messi");
+names.Add("Ronaldo");
+names.Add("Neymar");
+
+Generics<string>.PrintNames(names);
+
+
