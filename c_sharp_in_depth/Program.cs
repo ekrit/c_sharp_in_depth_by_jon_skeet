faa0b8394cc1d0e79362901d38dee91d37ab0cb4
haris.tiric <haris.tiric@weown.com>
2023-04-09 01:59:14 +0200

    iterators done

--- c_sharp_in_depth/Program.cs
+++ c_sharp_in_depth/Program.cs
@@ -4,11 +4,5 @@ using c_sharp_in_depth.Lessons;
 
 Console.WriteLine("Hello, World!");
 
-foreach (var one in Iterators.Fibonacci())
-{
-    Console.WriteLine(one);
-    if (one > 1000)
-        break;
-}
 
 
