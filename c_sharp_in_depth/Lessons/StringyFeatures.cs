261abe31c4dc294894c8221b031f989ac6d618a2
haris.tiric <haris.tiric@weown.com>
2023-07-16 08:07:35 +0200

    changes

--- c_sharp_in_depth/Lessons/StringyFeatures.cs
+++ c_sharp_in_depth/Lessons/StringyFeatures.cs
@@ -2,5 +2,32 @@
 
 public class StringyFeatures
 {
+    /*
+        public class Rectangle : INotifyPropertyChanged
+        {
+         public event PropertyChangedEventHandler PropertyChanged;
+         private double width;
+         private double height;
+         public double Width
+         {
+         get { return width; }
+         set
+         {
+         if (width == value) 
+         {
+         return;
+         }
+         width = value;
+         RaisePropertyChanged(); 
+         RaisePropertyChanged(nameof(Area)); 
+         }
+         }
+         public double Height { ... } 
+         public double Area => Width * Height; 
+         private void RaisePropertyChanged( 
+         [CallerMemberName] string propertyName = null) { ... }
+        }
+     */
+    
     
 }
