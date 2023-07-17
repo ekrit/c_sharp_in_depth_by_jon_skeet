54fc375e986a8d1ad15d098bca6084e6860ee62d
haris.tiric <haris.tiric@weown.com>
2023-07-17 06:48:11 +0200

    changes

---
+++ c_sharp_in_depth/Lessons/SmorgasboardFeatures.cs
@@ -0,0 +1,29 @@
+﻿namespace c_sharp_in_depth.Lessons;
+
+public class SmorgasboardFeatures
+{
+    // using static directives
+    
+    /*
+         static Point PolarToCartesian(double degrees, double magnitude)
+         {
+          double radians = degrees * Math.PI / 180; 
+          return new Point( 
+          Math.Cos(radians) * magnitude, 
+          Math.Sin(radians) * magnitude); 
+         } 
+          
+         after adding this line:
+          
+         using static System.Math;
+         
+         static Point PolarToCartesian(double degrees, double magnitude)
+         {
+          double radians = degrees * PI / 180; 
+          return new Point( 
+          Cos(radians) * magnitude, 
+          Sin(radians) * magnitude); 
+         }
+            
+     */
+}
