b5214186ba3bbe3bceb20a3490eabb4843aae672
haris.tiric <haris.tiric@weown.com>
2023-07-24 07:32:07 +0200

    changes

--- c_sharp_in_depth/Program.cs
+++ c_sharp_in_depth/Program.cs
@@ -4,7 +4,14 @@ using c_sharp_in_depth.Lessons;
 
 Console.WriteLine("Hello, World!");
 
+int[] values = { 2, 7, 3, -5, 1, 0, 10 };
+var extremes = Tuples.MinMax(values);
 
+var minimal = extremes.min;
+var maximal = extremes.max;
 
+Console.WriteLine(minimal); 
+Console.WriteLine(maximal);
 
+Console.WriteLine();
 
