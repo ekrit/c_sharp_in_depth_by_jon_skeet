eb054528fff3b0009f9eda3fc98f0bb96c4909b5
haris.tiric <haris.tiric@weown.com>
2023-08-03 06:50:46 +0200

    changes

--- c_sharp_in_depth/Program.cs
+++ c_sharp_in_depth/Program.cs
@@ -1,8 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Text.RegularExpressions;
 using c_sharp_in_depth.Lessons;
 
 Console.WriteLine("Hello, World!");
 
+Tuples.Match("hello");
+Tuples.Match(5L);
+Tuples.Match(7);
+Tuples.Match(10);
+Tuples.Match(10L);
 
- 
